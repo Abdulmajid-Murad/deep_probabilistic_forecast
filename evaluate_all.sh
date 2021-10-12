39b954e355dae21a93e66673dea501b7146d29d3
Murad <abdulmajid.a.murad@ntnu.no>
2021-10-13 00:28:18 +0200

    bash script to train and evaluate all models

--- evaluate_all.sh
+++ evaluate_all.sh
@@ -1,18 +1,18 @@
-# python main.py --model=NN_MC --mode=evaluate --task=regression
-# python main.py --model=NN_MC --mode=evaluate --task=classification
-# python main.py --model=NN_MC --mode=evaluate --task=regression --adversarial_training
-# python main.py --model=NN_MC --mode=evaluate --task=classification --adversarial_training
+python main.py --model=NN_MC --mode=evaluate --task=regression
+python main.py --model=NN_MC --mode=evaluate --task=classification
+python main.py --model=NN_MC --mode=evaluate --task=regression --adversarial_training
+python main.py --model=NN_MC --mode=evaluate --task=classification --adversarial_training
 
-# python main.py --model=Deep_Ensemble --mode=evaluate --task=regression
-# python main.py --model=Deep_Ensemble --mode=evaluate --task=classification
-# python main.py --model=Deep_Ensemble --mode=evaluate --task=regression --adversarial_training
-# python main.py --model=Deep_Ensemble --mode=evaluate --task=classification --adversarial_training
+python main.py --model=Deep_Ensemble --mode=evaluate --task=regression
+python main.py --model=Deep_Ensemble --mode=evaluate --task=classification
+python main.py --model=Deep_Ensemble --mode=evaluate --task=regression --adversarial_training
+python main.py --model=Deep_Ensemble --mode=evaluate --task=classification --adversarial_training
 
 
-# python main.py --model=SWAG --task=regression --mode=evaluate
-# python main.py --model=SWAG --task=regression --mode=evaluate  --adversarial_training
-# python main.py --model=SWAG --task=classification --mode=evaluate
-# python main.py --model=SWAG --task=classification --mode=evaluate  --adversarial_training
+python main.py --model=SWAG --task=regression --mode=evaluate
+python main.py --model=SWAG --task=regression --mode=evaluate  --adversarial_training
+python main.py --model=SWAG --task=classification --mode=evaluate
+python main.py --model=SWAG --task=classification --mode=evaluate  --adversarial_training
 
 
 python main.py --model=LSTM_MC --task=regression --mode=evaluate 
